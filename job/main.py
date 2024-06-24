9b5c4b04357dc1454a15af4635bf9cb675a878b6
HoangDH <daohuyhoang9119@gmail.com>
2024-06-24 11:36:06 +0700

    Update some code for etl

--- job/main.py
+++ job/main.py
@@ -1,6 +1,7 @@
 #Install libraries
 import os
 from dotenv import load_dotenv
+import pandas as pd
 import findspark
 findspark.init()
 import pyspark
@@ -11,11 +12,13 @@ import pyspark.sql.functions as f
 
 
 spark = SparkSession.builder.config("spark.driver.memory", "10g").getOrCreate()
+load_dotenv()
+input_path = os.getenv("INPUT_PATH")
+output_path = os.getenv("OUTPUT_PATH")
 
 
 def read_data(path, list_file):
     try:
-        df = spark.read.parquet(path)
         schema = StructType([
             StructField("datetime", StringType(), True),
             StructField("user_id", StringType(), True),
@@ -24,10 +27,18 @@ def read_data(path, list_file):
         ])
         data = spark.createDataFrame([],schema = schema)
         for i in list_file:
-            df = spark.read.parquet(path+i)
-            df = df.select('datetime','user_id','keyword','platform')
-            data = data.union(df)
-            print('Read data {}'.format(i))
+            # df = spark.read.parquet(path,i)
+            # df = df.select('datetime','user_id','keyword','platform')
+            # data = data.union(df)
+            # print('Read data {}'.format(i))
+            full_path = os.path.join(path, i)
+            try:
+                df = spark.read.parquet(full_path)
+                df = df.select('datetime', 'user_id', 'keyword', 'platform')
+                data = data.union(df)
+                print(f'Read data {full_path}')
+            except Exception as e:
+                print(f"Error reading file {full_path}: {e}")
         data = data.filter(data.user_id.isNotNull())
         data = data.filter(data.keyword.isNotNull())
         print('Data read finished')
@@ -37,7 +48,7 @@ def read_data(path, list_file):
         return None
     
 def transform_data(data):
-    data = data.select("datetime","user_id","keyword","platform","action")
+    data = data.select("datetime","user_id","keyword","platform")
     data = data.filter(data.user_id.isNotNull())
     data = data.filter(data.keyword.isNotNull())
     return data
@@ -61,34 +72,72 @@ def write_to_csv(df,output_path):
     except Exception as e:
         print(f"Error writing data to {output_path}: {e}")
 
+def save_distinct_values(df, column,output_file):
+    try:
+        distinct_values = df.select(column).distinct().rdd.map(lambda row: row[0]).collect()
+        
+        # Convert distinct values to a pandas DataFrame
+        distinct_df = pd.DataFrame(distinct_values, columns=[column])
+        
+        # Ensure output directory exists
+        output_dir = os.path.dirname(output_file)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        
+        # Write to CSV using pandas with UTF-8 encoding
+        distinct_df.to_csv(output_file, index=False, encoding='utf-8')
+        
+        print(f"Distinct values in {column} saved to {output_file}")
+    except Exception as e:
+        print(f"Error saving distinct values to {output_file}: {e}")
+
+
     
 def main(path):
+    print(path)
     print("---------Reading files from folder--------------")
     list_t6 = os.listdir(path)[0:2]
-    df = read_data(path,list_t6)
+    print(list_t6)
+
+    list_t7 = os.listdir(path)[2:]
+    print(list_t7)
 
 
-    print("---------Transform Data--------------")
-    # df = transform_data(df)
-    df.show(4, truncate=False)
+    print("---------Reading data month 6--------------")
+    df_6 = read_data(path,list_t6)
+    if df_6 is None:
+        print("Error reading data for month 6. Exiting.")
+        return
+    print("---------Reading data month 7--------------")
+    df_7 = read_data(path,list_t7)
+    if df_7 is None:
+        print("Error reading data for month 7. Exiting.")
+        return
 
-    print("---------convert Data--------------")
-    df = processing_most_search(df)
+
+    print("---------Transform Data month 6 & 7--------------")
+    df_6 = transform_data(df_6)
+    df_7 = transform_data(df_7)
+ 
+    print("---------Convert Data--------------")
+    df_6 = processing_most_search(df_6)
+    df_7 = processing_most_search(df_7)
+    df_6 = df_6.withColumnRenamed('Most_Search','Most_Search_T6')
+    df_7 = df_7.withColumnRenamed('Most_Search','Most_Search_T7')
+    df = df_6.join(df_7,'user_id','inner')
+    print("---------Result--------------")
     df.show(5,truncate=False)
 
+    # output_folder = "D:\\Đại Học CNTT\\Data engineer\\DE-COURSE\\Homework\\User Search Pipeline\\output\\month6\\"
+    # output_file = os.path.join(output_folder, "distinct_values_Most_Search_T6.csv")
+    # save_distinct_values(df, "Most_Search_T6", output_file)
 
-
-    # load_dotenv()
-    # output_path = os.getenv("OUTPUT_PATH")
-    # print("---------Write to csv--------------")
-    # write_to_csv(df, output_path)
-    
+    print("---------Write to csv--------------")
+    write_to_csv(df, output_path)
     
     print("---------Finish--------------")
     spark.stop()
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    input_path = os.getenv("INPUT_PATH")
     main(input_path)
